bf02e736bb6bc0073b7229805ffd41e7fb3f4216
tmfadhlul <tmfadhlul@gmail.com>
2021-09-22 16:49:46 +0700

    fix: cform not working

--- src/views/pages/register/Register.js
+++ src/views/pages/register/Register.js
@@ -1,4 +1,7 @@
-import React from 'react'
+/* eslint-disable prettier/prettier */
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+import { useForm } from 'react-hook-form'
 import {
   CButton,
   CCard,
@@ -15,6 +18,13 @@ import CIcon from '@coreui/icons-react'
 import { cilLockLocked, cilUser } from '@coreui/icons'
 
 const Register = () => {
+  // userform
+  const { register, handleSubmit } = useForm()
+
+  //on submit function
+  const onSubmit = (data) => {
+    console.log(data)
+  }
   return (
     <div className="bg-light min-vh-100 d-flex flex-row align-items-center">
       <CContainer>
@@ -22,41 +32,52 @@ const Register = () => {
           <CCol md={9} lg={7} xl={6}>
             <CCard className="mx-4">
               <CCardBody className="p-4">
-                <CForm>
-                  <h1>Register</h1>
-                  <p className="text-medium-emphasis">Create your account</p>
+                <h1>Register</h1>
+                <p className="text-medium-emphasis">Create your account</p>
+
+                {/* pemanggilan onSubmit */}
+                <CForm onSubmit={handleSubmit(onSubmit)}>
                   <CInputGroup className="mb-3">
                     <CInputGroupText>
                       <CIcon icon={cilUser} />
                     </CInputGroupText>
-                    <CFormInput placeholder="Username" autoComplete="username" />
+                    {/* regitrasi property */}
+                    <CFormInput {...register('name')} placeholder="Enter your name" />
                   </CInputGroup>
                   <CInputGroup className="mb-3">
                     <CInputGroupText>@</CInputGroupText>
-                    <CFormInput placeholder="Email" autoComplete="email" />
+                    {/* regitrasi property */}
+                    <CFormInput {...register('email')} placeholder="Enter your email" />
                   </CInputGroup>
                   <CInputGroup className="mb-3">
                     <CInputGroupText>
                       <CIcon icon={cilLockLocked} />
                     </CInputGroupText>
-                    <CFormInput
-                      type="password"
-                      placeholder="Password"
-                      autoComplete="new-password"
-                    />
+                    {/* regitrasi property */}
+                    <CFormInput {...register('email')} placeholder="Enter your email" />
                   </CInputGroup>
                   <CInputGroup className="mb-4">
                     <CInputGroupText>
                       <CIcon icon={cilLockLocked} />
                     </CInputGroupText>
                     <CFormInput
+                      {...register('password')}
                       type="password"
                       placeholder="Repeat password"
                       autoComplete="new-password"
                     />
                   </CInputGroup>
+
+                  <p style={{ color: '#2C384AF2' }}>
+                    Already have account?{' '}
+                    <Link to="/login" style={{ textDecoration: 'underline' }}>
+                      Login here
+                    </Link>
+                  </p>
                   <div className="d-grid">
-                    <CButton color="success">Create Account</CButton>
+                    <CButton type="submit" color="success">
+                      Create Account
+                    </CButton>
                   </div>
                 </CForm>
               </CCardBody>
